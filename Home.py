cd703b948a98f0fa0830c7b64e9a4e71b075f3a8
Aelzi <96818446+Aelzi@users.noreply.github.com>
2024-03-05 16:19:38 +0700

    update

--- Home.py
+++ Home.py
@@ -19,21 +19,13 @@ Dataset ini berisi data tentang penggunaan sepeda bersama di berbagai kota di se
 Data ini dapat digunakan untuk menganalisis berbagai aspek penggunaan sepeda bersama, 
 seperti pola penggunaan, pengaruh cuaca, dan faktor-faktor lain yang memengaruhi permintaan.
 
-**Beberapa contoh analisis yang dapat dilakukan dengan dataset ini:**
+**Beberapa contoh analisis yang akan dilakukan dengan dataset ini:**
 
-* Menganalisis pola penggunaan sepeda berdasarkan waktu, hari, dan bulan.
-* Mempelajari pengaruh cuaca dan musim terhadap penggunaan sepeda.
-* Mengidentifikasi lokasi yang memiliki permintaan tinggi untuk layanan berbagi sepeda.
-* Mengembangkan model untuk memprediksi permintaan sepeda di masa depan.
+* Bagaimana kondisi cuaca mempengaruhi jumlah peminjaman sepeda harian?
+* Kapan puncak peminjaman sepeda terjadi?
+* Bagaimana pola peminjaman sepeda berubah berdasarkan jam, hari, atau bulan?
 
-**Dataset ini dapat bermanfaat bagi berbagai pihak, seperti:**
 
-* Operator sistem berbagi sepeda
-* Pemerintah kota
-* Peneliti
-* Masyarakat umum
 
-**Anda dapat mengunduh dataset ini di sini:**
 
-[Link ke dataset]
 """)
--- pages/2_About_Dataset.py
+++ pages/2_About_Dataset.py
@@ -4,15 +4,25 @@ st.title("Bike Sharing Dataset")
 
 st.subheader("Latar Belakang")
 st.write("""
-Sistem berbagi sepeda adalah cara baru penyewaan sepeda tradisional, dimana seluruh proses mulai dari pendaftaran keanggotaan, penyewaan, dan pengembalian dilakukan secara otomatis..... (isi sisanya dari penjelasan dataset)
+Dataset Bike Sharing dibuat untuk memahami dan menganalisis sistem berbagi sepeda di Washington D.C., Amerika Serikat. Data ini dikumpulkan selama dua tahun (2011-2012) dan mencakup informasi tentang:
 """)
 
 st.subheader("Dataset")
-st.write("""  """)
-
-# Petunjuk penggunaan dataset secara umum (Anda bisa sesuaikan)
-st.subheader("Penggunaan")
-st.write("**Contoh Tugas yang Dapat Dilakukan:**")
-st.write("* **Regresi**: Prediksi jumlah penyewaan berdasarkan kondisi lingkungan dan musim.")
-st.write("* **Deteksi Peristiwa dan Anomali**")
-st
+st.write(""" 
+- **instant**: indeks baris
+- **dteday**: tanggal
+- **season**: musim (1: semi, 2: panas, 3: gugur, 4: dingin)
+- **yr**: tahun (0: 2011, 1:2012)
+- **mnth**: bulan (1 sampai 12)
+- **hr**: jam (0 sampai 23)
+- **holiday**: apakah hari itu libur atau tidak
+- **weekday**: hari
+- **workingday**: 1 jika bukan akhir pekan atau hari libur, 0 jika sebaliknya
+- **weathersit**: kondisi cuaca
+- **temp**: temperatur ternormalisasi (Celcius)
+- **atemp**: suhu yang dirasakan, ternormalisasi (Celcius)
+- **hum**: kelembapan ternormalisasi
+- **windspeed**: kecepatan angin ternormalisasi
+- **casual**: jumlah pengguna tidak terdaftar
+- **registered**: jumlah pengguna terdaftar
+- **cnt**: jumlah total sepeda yang disewa (termasuk terdaftar dan tidak) """)
