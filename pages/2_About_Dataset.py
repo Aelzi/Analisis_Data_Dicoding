bc6ad2a29bbd0b81141dcb137251ecb9ad96e861
Aelzi <96818446+Aelzi@users.noreply.github.com>
2024-03-05 13:03:21 +0700

    update

--- pages/2_About_Dataset.py
+++ pages/2_About_Dataset.py
@@ -1,39 +1,19 @@
-def example():
-    import streamlit as st
-    from prometheus_client import Counter
+import streamlit as st
+# Deskripsi Dataset
+st.title("Bike Sharing Dataset")
 
-    @st.cache_resource
-    def get_metric():
-        registry = streamlit_registry()
-        return Counter(
-            name="my_counter",
-            documentation="A cool counter",
-            labelnames=("app_name",),
-            registry=registry,  # important!!
-        )
+st.subheader("Latar Belakang")
+st.write("""
+Sistem berbagi sepeda adalah cara baru penyewaan sepeda tradisional, dimana seluruh proses mulai dari pendaftaran keanggotaan, penyewaan, dan pengembalian dilakukan secara otomatis..... (isi sisanya dari penjelasan dataset)
+""")
 
-    SLIDER_COUNT = get_metric()
+st.subheader("Dataset")
+st.write("""
+Dataset utama berisi catatan historis dua tahun (2011 dan 2012) dari sistem Capital Bikeshare di Washington D.C., Amerika Serikat.... (isi sisanya dari penjelasan dataset) 
+""")
 
-    app_name = st.text_input("App name", "prometheus_app")
-    latest = st.slider("Latest value", 0, 20, 3) 
-    if st.button("Submit"):
-        SLIDER_COUNT.labels(app_name).inc(latest)
-
-    st.write(
-        """
-        View a fuller example that uses the (safer) import metrics method at:
-        https://github.com/arnaudmiribel/streamlit-extras/tree/main/src/streamlit_extras/prometheus/example
-        """
-    )
-
-    st.write(
-        """
-        ### Example output at `{host:port}/_stcore/metrics`
-        ```
-        # TYPE my_counter counter
-        # HELP my_counter A cool counter
-        my_counter_total{app_name="prometheus_app"} 14.0
-        my_counter_created{app_name="prometheus_app"} 1.7042185907557938e+09
-        ```
-        """
-    )
+# Petunjuk penggunaan dataset secara umum (Anda bisa sesuaikan)
+st.subheader("Penggunaan")
+st.write("**Contoh Tugas yang Dapat Dilakukan:**")
+st.write("* **Regresi**: Prediksi jumlah penyewaan berdasarkan kondisi lingkungan dan musim.")
+st.write("* **Deteksi Peristiwa dan Anomali**")
